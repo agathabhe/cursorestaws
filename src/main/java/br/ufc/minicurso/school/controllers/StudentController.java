104a5a4137681732c50e77d7a1b22fa3d6732cec
Agatha <agathabmartinsr@gmail.com>
2019-04-04 18:39:22 -0300

    Servidor REST

---
+++ src/main/java/br/ufc/minicurso/school/controllers/StudentController.java
@@ -0,0 +1,16 @@
+package br.ufc.minicurso.school.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getStudents(){
+		return "Student API";
+	}
+
+}
